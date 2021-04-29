e5e2e028c8e04e4015766dbbe3f3a980b1e12ab5
Ian Hecker <ianhecker@yahoo.com>
2021-04-29 11:36:48 -0600

    Add bottom-up integration test (#11)
    * Add gen buildlist cmd to make
    
    * Add bottom-up integration test
    
    Co-authored-by: Ian Hecker <ian@blocky.rocks>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,9 +1,13 @@
 package integration_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+
+	"github.com/blocky/adlr"
+	"github.com/blocky/adlr/gotool"
 )
 
 type IntegrationTestSuite struct {
@@ -15,4 +19,46 @@ func TestIntegrationTestSuite(t *testing.T) {
 		t.Skip("Skip integration test")
 	}
 	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (suite IntegrationTestSuite) TestADLR() {
+	buildlist, err := os.Open("../buildlist.json")
+	if err != nil {
+		panic("no buildlist found")
+	}
+	defer buildlist.Close()
+
+	// unmarshal adlr's golang buildlist into modules
+	parser := gotool.MakeBuildListParser()
+	mods, err := parser.ParseModuleList(buildlist)
+	suite.Nil(err)
+
+	// filter out non-direct modules from the buildlist
+	direct := gotool.FilterDirectImportModules(mods)
+	deps := adlr.MakeDependencies(direct...)
+
+	// using the paths of the modules, find their licenses
+	prospector := adlr.MakeLicenseProspector()
+	prospects, err := prospector.ProspectLicenses(deps...)
+	suite.Nil(err)
+
+	// determine (best guess) module license specifics
+	miner := adlr.MakeLicenseMiner()
+	mined, err := miner.Mine(prospects...)
+	suite.Nil(err)
+
+	// create a license.lock with dependency licenses
+	licenselock := adlr.MakeLicenseLock("./")
+	err = licenselock.Lock(mined)
+	defer os.Remove("./" + adlr.LicenseLockName)
+	suite.Nil(err)
+
+	// vet license types with whitelist to ensure license
+	// requirement fulfillment
+	locks, err := licenselock.Read()
+	suite.Nil(err)
+	auditor := adlr.MakeLicenseAuditor()
+	suite.Nil(err)
+	err = auditor.AuditLocks(locks)
+	suite.Nil(err)
 }
