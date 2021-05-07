e3b0bb76685158ce31acf218142e58d60a08179d
Ian Hecker <ian@blocky.rocks>
2021-05-07 13:10:36 -0600

    Add api cmts for License

--- license.go
+++ license.go
@@ -1,10 +1,13 @@
 package adlr
 
+// License holds license details of a golang module
+// derived by licensedb and solved by Miner
 type License struct {
 	Kind string `json:"kind"`
 	Text string `json:"text"`
 }
 
+// Create a License
 func MakeLicense(
 	kind, text string,
 ) License {
