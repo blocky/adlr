0a30d3e554c86300937e00dc4fff646855986a04
Ian Hecker <ian@blocky.rocks>
2021-05-07 13:49:25 -0600

    Add api cmts for Whitelist

--- whitelist.go
+++ whitelist.go
@@ -2,11 +2,13 @@ package adlr
 
 import "sort"
 
-// The following list follows SPDX License Identifier standards
-// To add an ACCEPTABLE LICENSE to this list, see:
+// This list is of SPDX License Identifiers, the standard
+// used by the text-mining package:
+// github.com/go-enry/go-license-detector/v4
+// and contains licenses deemed automatically fulfillable.
+// To add to this list, see:
 // https://spdx.org/licenses/
-// for the license identifier
-
+// for license identifiers
 var Whitelist = []string{
 	"Apache-2.0",
 	"BSD-1-Clause",
@@ -16,15 +18,20 @@ var Whitelist = []string{
 	"MIT-0",
 }
 
+// LicenseWhitelist is a whitelist of automatically fulfillable
+// licenses, with search by license type
 type LicenseWhitelist struct {
 	whitelist []string
 	init      bool
 }
 
+// Create a LicenseWhitelist with default values
 func MakeLicenseWhitelist() LicenseWhitelist {
 	return MakeLicenseWhitelistFromRaw(Whitelist)
 }
 
+// Create a LicenseWhitelist from specified values.
+// Initialize whitelist for searching
 func MakeLicenseWhitelistFromRaw(
 	whitelist []string,
 ) LicenseWhitelist {
@@ -32,6 +39,7 @@ func MakeLicenseWhitelistFromRaw(
 	return LicenseWhitelist{whitelist, init}
 }
 
+// Search a whitelist of license types that a license exists
 func (lw LicenseWhitelist) Find(
 	license string,
 ) bool {
@@ -42,8 +50,10 @@ func find(
 	list []string,
 	x string,
 ) bool {
+	// return index i to insert item x
+	// x exists if list[i] == x
 	i := sort.SearchStrings(list, x)
-	if i == len(list) { // if after every list item
+	if i >= len(list) {
 		return false
 	} else if x != list[i] {
 		return false
