df2fdcb21865e6ee4c6f1a1358bc5e19db391a92
Ian Hecker <ian@blocky.rocks>
2021-05-10 10:48:25 -0600

    Simplify build sh

--- sh/build.sh
+++ sh/build.sh
@@ -2,27 +2,9 @@
 
 set -e
 
-output_name(){
-    name=$1
-    dist=$2
-    output=$dist/$name'-'$os'-'$arch
-
-    if [[ $os = "windows" ]];
-    then
-        output+='.exe'
-    fi
-    echo $output
-}
-
-serialize_licenselock(){
-    # ldflags cannot have newlines or spaces
-    licenselock=$1
-    sed 's/\s/\\s/g' $licenselock | tr -d '\n'
-}
-
 # process arguments
 if [[ "$#" -ne 6 ]]; then
-    echo "usage: $0 name os arch src dist depreq"
+    echo "usage: $0 name os arch src dist lock"
     echo "given: $0 $@"
     echo "  Valid arch and os values are those supported by GOARCH and GOOS"
     exit 1
@@ -33,15 +15,18 @@ os=$2
 arch=$3
 src=$4
 dist=$5
-depreq=$6
+lock=$6
 
-output=$(output_name $name $dist)
+output=$dist/$name'-'$os'-'$arch
+if [[ $os = "windows" ]];
+then
+    output+='.exe'
+fi
 
-serialized=$(serialize_licenselock $6)
-depreq_flag="-X main.DependencyRequirements=$serialized"
+serialized=$(sed 's/\s/\\s/g' $lock | tr -d '\n')
 
 printf "Building %s...\n" $output
 env GCO_ENABLED=0 GOOS=$os GOARCH=$arch \
 go build \
-    -ldflags "$depreq_flag" \
+    -ldflags "-X main.DependencyRequirements=$serialized" \
     -o $output $src
