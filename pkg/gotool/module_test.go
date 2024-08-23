1a34c0deead48d5eb99b661068f35295e0324589
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2024-08-23 09:11:57 -0400

    Update FilterImportModules test to look for indirect and redirect modules as well

--- pkg/gotool/module_test.go
+++ pkg/gotool/module_test.go
@@ -20,10 +20,30 @@ var ModuleStr = `{` +
 	`"Dir":"/home/user/path"}`
 
 var Modules = []gotool.Module{
-	gotool.Module{
-		Path:    "github.com/spf13/cobra",
-		Version: "v0.0.5",
-		Dir:     "/home/user/go/pkg/mod/github.com/spf13/cobra@v0.0.5",
+	{
+		Path:    "bky.sh/pallets/nub",
+		Version: "v0.0.0",
+		Replace: &gotool.Module{
+			Path: "../nub",
+			Dir:  "/home/user/Work/bky.sh/pallets/nub",
+		},
+		Main:     false,
+		Indirect: false,
+		Dir:      "/home/user/Work/bky.sh/pallets/nub",
+	},
+	{
+		Path:     "cirello.io/pglock",
+		Version:  "v1.8.1-0.20200922151210-b76be34db4ac",
+		Main:     false,
+		Indirect: true,
+		Dir:      "/home/user/go/pkg/mod/cirello.io/pglock@v1.8.1-0.20200922151210-b76be34db4ac",
+	},
+	{
+		Path:     "github.com/spf13/cobra",
+		Version:  "v0.0.5",
+		Main:     false,
+		Indirect: false,
+		Dir:      "/home/user/go/pkg/mod/github.com/spf13/cobra@v0.0.5",
 	},
 }
 
@@ -52,23 +72,23 @@ func TestModuleUnmarshal(t *testing.T) {
 	})
 }
 
-func TestFilterDirectImportModules(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		f, _ := os.Open(TestDirModule + "directimports.json")
+func TestFilterImportModules(t *testing.T) {
+	t.Run("happy path - filter all but main", func(t *testing.T) {
+		f, _ := os.Open(TestDirModule + "all_imports.json")
 		p := gotool.MakeBuildListParser()
 		list, err := p.ParseModuleList(f)
 		assert.Nil(t, err)
 
-		result := gotool.FilterDirectImportModules(list)
+		result := gotool.FilterImportModules(list)
 		assert.Equal(t, Modules, result)
 	})
-	t.Run("empty on no direct imports", func(t *testing.T) {
-		f, _ := os.Open(TestDirModule + "indirectimports.json")
+	t.Run("happy path - empty on only main import", func(t *testing.T) {
+		f, _ := os.Open(TestDirModule + "main_import.json")
 		p := gotool.MakeBuildListParser()
 		list, err := p.ParseModuleList(f)
 		assert.Nil(t, err)
 
-		result := gotool.FilterDirectImportModules(list)
+		result := gotool.FilterImportModules(list)
 		assert.Equal(t, []gotool.Module(nil), result)
 	})
 }
