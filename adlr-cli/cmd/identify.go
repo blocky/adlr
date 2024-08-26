2a123a14e4a9ee5c9966acc06fcae4ba1473ea7f
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2024-08-26 03:13:28 -0400

    Add cli args for configuring miner confidence levels during license identification process

--- adlr-cli/cmd/identify.go
+++ adlr-cli/cmd/identify.go
@@ -6,10 +6,14 @@ import (
 	"os"
 
 	"github.com/blocky/adlr"
+	"github.com/blocky/adlr/pkg/ascertain"
+	"github.com/blocky/adlr/pkg/reader"
 	"github.com/spf13/cobra"
 )
 
 var IdentifiedFile string
+var Confidence float32
+var Lead float32
 
 var identifyCmd = &cobra.Command{
 	Use:   "identify",
@@ -31,7 +35,13 @@ func init() {
 		&IdentifiedFile, "identified", "i", "./identified-licenses.json",
 		"Output file containing identified licenses",
 	)
-
+	identifyCmd.Flags().Float32VarP(
+		&Confidence, "confidence", "c", ascertain.Confidence,
+		"Minimum required confidence for a license match from text mining",
+	)
+	identifyCmd.Flags().Float32VarP(
+		&Lead, "lead", "d", ascertain.Lead,
+		"Minimum required difference between primary match and secondary matches")
 	licenseCmd.AddCommand(identifyCmd)
 }
 
@@ -53,7 +63,8 @@ func Identify(
 	}
 
 	unidentified := ""
-	identified, err := adlr.MakeMiner().Mine(located...)
+	lr := reader.NewLimitedReaderFromRaw(reader.Kilobyte * 1000)
+	identified, err := adlr.MakeMinerFromRaw(Confidence, Lead, lr).Mine(located...)
 	if err != nil {
 		unidentified = err.Error()
 	}
